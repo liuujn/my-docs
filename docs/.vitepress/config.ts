782a4eecb972f997a419cd187692620f7d935e32
刘俊3 <liujun3@gcsoft.com>
2025-10-21 16:20:32 +0800

    Updates

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+     nav: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about' }
+    ]
+})
+
