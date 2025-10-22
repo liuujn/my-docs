abd5eec81777b15a726af68e06036561a61b2102
刘俊3 <liujun3@gcsoft.com>
2025-10-22 13:57:01 +0800

    feat: 更新了内容

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,17 +3,18 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   base: '/my-docs/',
   themeConfig: {
+     siteTitle: '我的文档',
     nav: [
-      { text: '首页', link: '/' },        // ✅ 正确：VitePress 会自动添加 base
-      { text: '关于', link: '/about' }    // ✅ 正确：VitePress 会自动添加 base
+      { text: 'Java', link: '/' },      
+      { text: 'c#', link: '/about' }  
     ],
     
     sidebar: [
       {
         text: '指南',
         items: [
-          { text: '首页', link: '/' },    // ✅ 正确
-          { text: '关于', link: '/about' } // ✅ 正确
+          { text: '首页', link: '/' },    
+          { text: '关于', link: '/about' } 
         ]
       }
     ]
