5a6daaaf6cac228541ef29ff611fa629e48d9555
刘俊3 <liujun3@gcsoft.com>
2025-10-30 15:34:48 +0800

    feat: 更新了内容

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
     siteTitle: '我的文档',
     nav: [
       { text: 'Java教程', link: '/java/' }, 
-      { text: 'C#教程', link: '/csharp/' },
     ],
     
     sidebar: {
@@ -24,24 +23,6 @@ export default defineConfig({
         },
       ],
       
-      '/csharp/': [
-        {
-          text: 'C#基础',
-          items: [
-            { text: '语法基础', link: '/csharp/basics' },
-            { text: '面向对象', link: '/csharp/oop' },
-            { text: 'LINQ查询', link: '/csharp/linq' }
-          ]
-        },
-        {
-          text: '.NET框架',
-          items: [
-            { text: 'ASP.NET Core', link: '/csharp/aspnet-core' },
-            { text: 'Entity Framework', link: '/csharp/entity-framework' },
-            { text: 'Web API', link: '/csharp/web-api' }
-          ]
-        }
-      ],
     },
     
     socialLinks: [
